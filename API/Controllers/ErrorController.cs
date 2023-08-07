b691d80fdf81ed869966c780ba9dce1fb80175d0
Szabolcs Peter <szabi.peter@gmail.com>
2023-08-07 13:39:15 +0200

    API Error handling

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("errors/{code}")]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
+
+// For this to be called we have to use middleware in Program.cs
